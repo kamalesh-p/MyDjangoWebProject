61a7dbb931d4a844bbf6de7cedbf16fabda1d39a
kamalesh-p <66113370+kamalesh-p@users.noreply.github.com>
2020-06-14 18:26:14 +0530

    Add files via upload

---
+++ MyDjangoWebProject/MyDjangoWebProject/urls.py
@@ -0,0 +1,11 @@
+"""
+Definition of urls for MyDjangoWebProject.
+"""
+
+from django.urls import path, include
+from django.contrib import admin
+
+urlpatterns = [
+    path('',include('app.urls')),
+    path('admin/', admin.site.urls),
+]
